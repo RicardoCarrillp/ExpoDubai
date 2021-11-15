5855a642f57d44330205d1f29c6e809d3f9ff9ae
elian-vega-h <andres.v.h@hotmail.com>
2021-11-15 18:37:05 -0500

    utb at glance

--- src/views/utbAtG.js
+++ src/views/utbAtG.js
@@ -1,10 +1,41 @@
+import FinalNavbar from 'components/Navbars/FinalNavbar';
 import React from 'react'
+import img0 from '../img/1.jpeg'
+import img1 from '../img/2.jpeg'
+import img2 from '../img/3.jpeg'
+import img3 from '../img/4.jpeg'
+import img4 from '../img/5.jpeg'
+import img5 from '../img/6.jpeg'
+import img6 from '../img/7.jpeg'
+///import '../assets/scss/paper-kit/_utbatg.scss'
+
+
 
 function utbAtG() {
     return (
-      <div>
-        <h2>soy Utb at glance</h2>
+      
+   
+      <div class="utbatg">
+        <FinalNavbar/>
+
+
+      
+        <div>
+
+          <img src={img0} alt="" />
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+          <img src={img4} alt="" />
+          <img src={img5} alt="" /> 
+          <img src={img6} alt="" />             
+        </div>
+
+        
       </div>
+
+
+
     );
 }
 
