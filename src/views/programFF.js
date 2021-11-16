18cad49cb72a3cb8fba241b73a532e22431012d9
RicardoCarrillp <60947627+RicardoCarrillp@users.noreply.github.com>
2021-11-16 14:13:14 -0500

    Program

--- src/views/programFF.js
+++ src/views/programFF.js
@@ -1,13 +1,16 @@
-
-
 import React from 'react'
 import {
 
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  CardFooter,
   Container,
   Row,
   Col,
 } from "reactstrap";
-import ReactPlayer from "react-player";
+//import ReactPlayer from "react-player";
 
 // core components
 import FinalNavbarAboutTeam from "components/Navbars/FinalNavbarAboutTeam.js";
@@ -24,9 +27,161 @@ function programFF() {
           <br />
           <Container>
             <Row>
-             
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="title">Programs for foreigners</h2>
+                <h5 className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
+                  leo ut purus semper tempus. 
+                </h5>
+                <br />
+              </Col>
             </Row>
           </Container>
+          <br />
+          <Container>
+          <Row>
+            <Col md="3">
+              <Card className="card-profile card-plain">
+                <div className="card-avatar">
+                  <p>
+                    <img
+                      alt="..."
+                      src={require("assets/img/fabio-mangione.jpg").default}
+                    />
+                  </p>
+                </div>
+                <CardBody>
+                  <p>
+                    <div className="author">
+                      <CardTitle tag="h4">Summer school</CardTitle>
+                      <h6 className="card-category">Courses</h6>
+                    </div>
+                  </p>
+                  <p className="card-description text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
+                    leo ut purus semper tempus.
+                  </p>
+                </CardBody>
+                <CardFooter className="text-center">
+                  <Button
+                    className="btn-just-icon btn-neutral"
+                    color="link"
+                    href="https://www.instagram.com/rickicarrillo22/?hl=es"
+                  >
+                    <i className="fa fa-instagram" />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="3">
+              <Card className="card-profile card-plain">
+                <div className="card-avatar">
+                  <p>
+                    <img
+                      alt="..."
+                      src={require("assets/img/fabio-mangione.jpg").default}
+                    />
+                  </p>
+                </div>
+                <CardBody>
+                  <p>
+                    <div className="author">
+                      <CardTitle tag="h4">Spanish courses</CardTitle>
+                      <h6 className="card-category">Courses</h6>
+                    </div>
+                  </p>
+                  <p className="card-description text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
+                    leo ut purus semper tempus.
+                  </p>
+                </CardBody>
+                <CardFooter className="text-center">
+                  <Button
+                    className="btn-just-icon btn-neutral"
+                    color="link"
+                    href="#pablo"
+                  >
+                    <i className="fa fa-instagram" />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="3">
+              <Card className="card-profile card-plain">
+                <div className="card-avatar">
+                  <p>
+                    <img
+                      alt="..."
+                      src={require("assets/img/fabio-mangione.jpg").default}
+                    />
+                  </p>
+                </div>
+                <CardBody>
+                  <p>
+                    <div className="author">
+                      <CardTitle tag="h4">Continuing education</CardTitle>
+                      <h6 className="card-category">Courses</h6>
+                    </div>
+                  </p>
+                  <p className="card-description text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
+                    leo ut purus semper tempus.
+                  </p>
+                </CardBody>
+                <CardFooter className="text-center">
+                  <Button
+                    className="btn-just-icon btn-neutral"
+                    color="link"
+                    href="#pablo"
+                  >
+                    <i className="fa fa-instagram" />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="3">
+              <Card className="card-profile card-plain">
+                <div className="card-avatar">
+                  <p>
+                    <img
+                      alt="..."
+                      src={
+                        require("assets/img/fabio-mangione.jpg").default
+                      }
+                    />
+                  </p>
+                </div>
+                <CardBody>
+                  <p>
+                    <div className="author">
+                      <CardTitle tag="h4">Scholarships</CardTitle>
+                      <h6 className="card-category">Courses</h6>
+                    </div>
+                  </p>
+                  <p className="card-description text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
+                    leo ut purus semper tempus.
+                  </p>
+                </CardBody>
+                <CardFooter className="text-center">
+                  <Button
+                    className="btn-just-icon btn-neutral"
+                    color="link"
+                    href="#pablo"
+                  >
+                    <i className="fa fa-instagram" />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+        </Container>
           <br /> <br />
           <DemoFooter />
         </>
