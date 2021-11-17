5f26da942f52f5bda08f6284ae2d98ccf4b23959
RicardoCarrillp <60947627+RicardoCarrillp@users.noreply.github.com>
2021-11-17 14:27:11 -0500

    iframe

--- src/views/aboutCtg.js
+++ src/views/aboutCtg.js
@@ -1,6 +1,8 @@
+import DemoFooter from 'components/Footers/DemoFooter';
 import AboutCtgHeader from 'components/Headers/aboutctgHeader';
 import FinalNavbarAboutTeam from 'components/Navbars/FinalNavbarAboutTeam';
 import React from 'react'
+import Iframe from 'react-iframe'
 import { Container, Row, Col } from "reactstrap";
 function aboutCtg() {
     return (
@@ -11,58 +13,38 @@ function aboutCtg() {
           <br />
           <Container>
             <Row>
-              <Col className="ml-auto mr-auto" md="11">
+              <Col className="ml-auto mr-auto" md="12">
                 <h2 className="title">Cartagena</h2>
                 <h5 className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Pellentesque dapibus tempus sodales. Ut convallis vulputate
-                  eros. Curabitur vel turpis imperdiet augue feugiat suscipit
-                  quis vel mi. Aenean ullamcorper dolor est, in tincidunt mi
-                  finibus sed. Etiam suscipit massa sit amet turpis condimentum,
-                  non ultrices dui commodo. Praesent ac tellus volutpat augue
-                  commodo pharetra. Quisque varius pharetra lacus sit amet
-                  fermentum. Mauris condimentum non nibh eu viverra. Maecenas
-                  aliquam sagittis enim. Suspendisse potenti. Suspendisse
-                  vehicula vulputate venenatis. Nullam cursus luctus mauris, id
-                  convallis massa laoreet vitae. Donec ipsum augue, tincidunt
-                  nec dolor vel, porta venenatis nunc. Sed lacus turpis, laoreet
-                  eu sapien nec, auctor feugiat dui. Etiam ut gravida lectus, ut
-                  ornare magna. Donec interdum vitae lorem eget fermentum. Nam
-                  non ligula sed dolor cursus tristique in ac ligula. Etiam
-                  dignissim lorem vitae ipsum faucibus fringilla. Sed egestas
-                  quam ac sem semper imperdiet. Suspendisse blandit nunc ut est
-                  ultricies posuere. Fusce ligula massa, aliquam vel dictum in,
-                  accumsan quis erat. Pellentesque consectetur et elit id
-                  vestibulum. Nunc a lacinia nisl, eu vestibulum libero.
-                  Suspendisse potenti. Pellentesque aliquet et dui vel
-                  fringilla. Sed suscipit ultricies vestibulum. Pellentesque
-                  sapien tellus, feugiat euismod massa ac, placerat dictum mi.
-                  Vivamus dictum sem a ex vulputate tincidunt. Nunc ornare
-                  feugiat risus, eget condimentum nulla auctor id. Quisque et
-                  turpis nec purus pellentesque dignissim a a justo. Morbi vel
-                  arcu libero. Orci varius natoque penatibus et magnis dis
-                  parturient montes, nascetur ridiculus mus. Pellentesque
-                  hendrerit, urna eu gravida mollis, lacus elit gravida ipsum,
-                  non hendrerit ligula orci sit amet turpis. Mauris ac enim
-                  viverra, elementum nisl quis, tincidunt ex. Pellentesque
-                  pharetra nec felis eget faucibus. Nulla eros diam, sagittis eu
-                  arcu at, ultricies vehicula est. Etiam rhoncus non libero et
-                  vulputate. Vivamus euismod massa et ipsum pulvinar, vitae
-                  viverra nunc efficitur. Aliquam semper dui a faucibus varius.
-                  In congue semper lacus, sed auctor urna accumsan et. Maecenas
-                  laoreet leo at ligula pellentesque, aliquam vestibulum mi
-                  lobortis. Curabitur at ante aliquet, interdum odio ut,
-                  ultricies mauris.
-                  Enyoy!! 
-                  <a href="https://www.youvisit.com/tour/cartagena">
-                    <strong> https://www.youvisit.com/tour/cartagena.</strong>
-                  </a>
+                  Named a UNESCO World Heritage Site in 1984, it is no surprise
+                  that Cartagena is the birthplace of Gabriel García Márquez's
+                  magical realism and the jewel in the crown of Colombia's
+                  Caribbean coast. The exuberant atmosphere of the city, its
+                  colonial architecture and the abundance of cultural activities
+                  provide the perfect setting for students, teachers and
+                  visitors interested in an unforgettable academic and cultural
+                  experience. "And, over time, when I have to retrieve memories,
+                  I will always bring an incident from Cartagena, a place in
+                  Cartagena, a character in Cartagena."
+                  <strong>Gabriel García Márquez</strong> <br />
+                  Enyoy!
                 </h5>
                 <br />
+                <Iframe
+                  src="https://www.youvisit.com/tour/cartagena"
+                  frameborder="0"
+                  allowfullscreen
+                  width="100%"
+                  height="120%"
+                ></Iframe>
+               
               </Col>
             </Row>
           </Container>
+          <br />
+          <br />{" "}
         </>
+     
       </>
     );
 }
--- src/views/aboutUtb.js
+++ src/views/aboutUtb.js
@@ -1,10 +1,5 @@
-import React from 'react'
-import {
-
-  Container,
-  Row,
-  Col,
-} from "reactstrap";
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
 import ReactPlayer from "react-player";
 
 // core components
@@ -13,54 +8,74 @@ import UtbBannerHeader from "components/Headers/utbbannerHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
 
 function aboutUtb() {
-
-    return (
+  return (
+    <>
       <>
-        <>
-          <FinalNavbarAboutTeam />
-          <UtbBannerHeader />
-          <br />
-          <Container>
-            <Row>
-              <Col className="ml-auto mr-auto" md="8">
-                <h2 className="title">UTB</h2>
-                <h5 className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam ut convallis mauris, quis accumsan elit. Etiam dictum
-                  leo ut purus semper tempus. Donec elit ex, elementum vitae
-                  arcu sit amet, mattis iaculis magna. Sed et molestie arcu.
-                  Duis dictum, mauris at fermentum finibus, eros odio cursus
-                  nisl, ac rutrum magna enim eget mi. Mauris consectetur mattis
-                  convallis. Phasellus accumsan purus lacus, sed tristique dolor
-                  bibendum et. Vivamus eget vehicula lacus. Sed ut purus vitae
-                  neque pretium porta. Nulla facilisi. Donec eget urna ut metus
-                  sodales luctus id non nunc. Sed convallis mattis justo, in
-                  tristique nisi. Nullam non est euismod, bibendum felis eget,
-                  porttitor mi. Fusce vitae dui in justo hendrerit euismod vel
-                  lobortis odio. Sed ultrices sed dolor non commodo.
-                </h5>
+        <FinalNavbarAboutTeam />
+        <UtbBannerHeader />
+        <br />
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" md="8">
+              <h2 className="title">UNIVERSIDAD TECNOLOGICA DE BOLIVAR </h2>
+              <h5 className="description">
+                The Universidad Tecnologica de Bolivar is a private, non-profit
+                university founded in 1970 and located in Cartagena de Indias,
+                Colombia, a historical and cultural heritage site of humanity.
+                The University, also known as UTB, is a regional university
+                managed by the most important business associations in the city.
+                Their purpose is “change lives with excellence education”. The
+                University is recognized by their focus on inclusion, social
+                leadership and innovation.
                 <br />
-              </Col>
-            </Row>
-          </Container>
-          <Container>
-            <Row>
-              <Col className="ml-auto mr-auto" md="12">
-                <ReactPlayer
-                  url="https://www.youtube.com/watch?v=SKmgT9wvcXY"
-                  className="react-player"
-                  width="100%"
-                  playing="true"
-                  height="70vh"
-                />
-              </Col>
-            </Row>
-          </Container>
-          <br /> <br />
-          <DemoFooter />
-        </>
+                <strong>UTB</strong> was the first institution in the city that
+                received Institutional Accreditation, which is the highest
+                quality recognition granted by the Ministry of National
+                Education to higher education institutions in Colombia.
+                <br />
+                <strong>Board of Directors:</strong> UTB is managed by the most
+                important business associations in the region:
+                <ul>
+                  <li>
+                    <strong>ANDI</strong>(National Industry Association)
+                  </li>
+                  <li>
+                    <strong>Camacol</strong> (Colombian Builders Chamber)
+                  </li>
+                  <li>
+                    <strong>Cartagena Chamber of Commerce</strong>
+                  </li>
+                  <li>
+                    <strong>Fenalco</strong> (National Trader’s Federation)
+                  </li>
+                  <li>
+                    <strong>ACOPI</strong> (Colombian Small and Medium Business
+                    Association)
+                  </li>
+                </ul>
+              </h5>
+              <br />
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" md="12">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=SKmgT9wvcXY"
+                className="react-player"
+                width="100%"
+                playing="true"
+                height="70vh"
+              />
+            </Col>
+          </Row>
+        </Container>
+        <br /> <br />
+        <DemoFooter />
       </>
-    );
+    </>
+  );
 }
 
 export default aboutUtb;
